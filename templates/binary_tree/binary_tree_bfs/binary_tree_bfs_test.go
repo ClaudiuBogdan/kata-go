6f168f97c81c6305600b6980692687ab590e87a2
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-09-09 16:06:28 +0300

    feat(templates): update binary tree bfs test

--- templates/binary_tree/binary_tree_bfs/binary_tree_bfs_test.go
+++ templates/binary_tree/binary_tree_bfs/binary_tree_bfs_test.go
@@ -6,46 +6,73 @@ import (
 )
 
 func TestBFS(t *testing.T) {
-	tests := []struct {
-		name     string
-		tree     *TreeNode
-		expected []int
-	}{
-		{
-			name: "Normal binary tree",
-			tree: &TreeNode{
-				Val: 1,
-				Left: &TreeNode{
-					Val:   2,
-					Left:  &TreeNode{Val: 4},
-					Right: &TreeNode{Val: 5},
-				},
-				Right: &TreeNode{
-					Val:   3,
-					Left:  &TreeNode{Val: 6},
-					Right: &TreeNode{Val: 7},
-				},
-			},
-			expected: []int{1, 2, 3, 4, 5, 6, 7},
-		},
-		{
-			name:     "Empty tree",
-			tree:     nil,
-			expected: []int{},
-		},
-		{
-			name:     "Tree with only root",
-			tree:     &TreeNode{Val: 1},
-			expected: []int{1},
-		},
-	}
+    tests := []struct {
+        name     string
+        tree     *TreeNode
+        expected []int
+    }{
+        {
+            name: "Normal binary tree",
+            tree: &TreeNode{
+                Val: 1,
+                Left: &TreeNode{
+                    Val:   2,
+                    Left:  &TreeNode{Val: 4},
+                    Right: &TreeNode{Val: 5},
+                },
+                Right: &TreeNode{
+                    Val:   3,
+                    Left:  &TreeNode{Val: 6},
+                    Right: &TreeNode{Val: 7},
+                },
+            },
+            expected: []int{1, 2, 3, 4, 5, 6, 7},
+        },
+        {
+            name:     "Empty tree",
+            tree:     nil,
+            expected: []int(nil),
+        },
+        {
+            name:     "Tree with only root",
+            tree:     &TreeNode{Val: 1},
+            expected: []int{1},
+        },
+        {
+            name: "Unbalanced tree",
+            tree: &TreeNode{
+                Val: 1,
+                Left: &TreeNode{
+                    Val: 2,
+                    Left: &TreeNode{
+                        Val: 4,
+                        Left: &TreeNode{Val: 7},
+                    },
+                },
+                Right: &TreeNode{Val: 3},
+            },
+            expected: []int{1, 2, 3, 4, 7},
+        },
+        {
+            name: "Tree with negative values",
+            tree: &TreeNode{
+                Val: -1,
+                Left: &TreeNode{
+                    Val:   -2,
+                    Right: &TreeNode{Val: 5},
+                },
+                Right: &TreeNode{Val: -3},
+            },
+            expected: []int{-1, -2, -3, 5},
+        },
+    }
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			result := BFS(tt.tree)
-			if !reflect.DeepEqual(result, tt.expected) {
-				t.Errorf("BFS() = %v, want %v", result, tt.expected)
-			}
-		})
-	}
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            result := BFS(tt.tree)
+            if !reflect.DeepEqual(result, tt.expected) {
+                t.Errorf("BFS() = %v, want %v", result, tt.expected)
+            }
+        })
+    }
 }
