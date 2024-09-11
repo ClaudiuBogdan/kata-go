00192fea44314a31f38df8de04ee51131b65eb30
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-09-11 11:09:37 +0300

    feat(day3): solve coin change

---
+++ practice/day_3/coin_change/coin_change.go
@@ -0,0 +1,33 @@
+package coinchange
+
+func CoinChange(coins []int, amount int) int {
+	dp := make([]int, amount + 1)
+
+	for i := range dp {
+		dp[i] = amount + 1 // We can't use MaxInt value. Watch out for overflow: dp[i - coin] + 1
+	}
+
+	// Base case
+	dp[0] = 0
+
+	// Fil the dp table
+	for _, coin := range coins{
+		for i := coin; i <= amount; i++{
+			dp[i] = min(dp[i], dp[i - coin] + 1)
+		}
+	}
+
+	if dp[amount] > amount {
+		return -1
+	}
+
+	return dp[amount]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
