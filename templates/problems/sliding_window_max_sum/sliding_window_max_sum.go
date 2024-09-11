6316bd49d29d9f1064d1261f8d31a730f1f58a1b
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-09-11 08:50:10 +0300

    feat(templates): update max sum

--- templates/problems/sliding_window_max_sum/sliding_window_max_sum.go
+++ templates/problems/sliding_window_max_sum/sliding_window_max_sum.go
@@ -2,25 +2,5 @@ package slidingwindow
 
 // MaxSubarraySum finds the maximum subarray sum using the sliding window technique
 func MaxSubarraySum(arr []int) int {
-    if len(arr) == 0 {
-        return 0
-    }
-
-    maxSum := arr[0]
-    currentSum := 0
-
-    for _, num := range arr {
-        currentSum = max(num, currentSum+num)
-        maxSum = max(maxSum, currentSum)
-    }
-
-    return maxSum
-}
-
-// max returns the maximum of two integers
-func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+    
 }
