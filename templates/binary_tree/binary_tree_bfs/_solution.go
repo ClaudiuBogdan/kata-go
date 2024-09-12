646c5519601e708bd9edb79b1761856b9cfab212
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-09-12 18:38:28 +0300

    remove(templates): binary tree traversal, it's already present as dfs

---
+++ templates/binary_tree/binary_tree_bfs/_solution.go
@@ -0,0 +1,33 @@
+package binarytreebfs
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func BFS(root *TreeNode) []int {
+	if root == nil {
+		return []int(nil)
+	}
+
+	var result []int
+
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		result = append(result, node.Val)
+
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+	}
+
+	return result
+}
