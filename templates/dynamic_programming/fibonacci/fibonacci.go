b49aa311be048fdf40eff6785a336640c82c3068
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-09-12 18:45:57 +0300

    feat(templates): update fibonacci

--- templates/dynamic_programming/fibonacci/fibonacci.go
+++ templates/dynamic_programming/fibonacci/fibonacci.go
@@ -2,43 +2,10 @@ package fibonacci
 
 // GenerateFibonacci generates the Fibonacci sequence up to n terms using dynamic programming
 func GenerateFibonacci(n int) []int {
-	if n <= 0 {
-		return []int{}
-	}
-	if n == 1 {
-		return []int{0}
-	}
-	if n == 2 {
-		return []int{0, 1}
-	}
 
-	dp := make([]int, n)
-	dp[0] = 0
-	dp[1] = 1
-
-	for i := 2; i < n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
-	}
-
-	return dp
 }
 
 // FibonacciTerm calculates the nth term of the Fibonacci sequence
 func FibonacciTerm(n int) int {
-	if n <= 0 {
-		return 0
-	}
-	if n == 1 {
-		return 0
-	}
-	if n == 2 {
-		return 1
-	}
 
-	prev, curr := 0, 1
-	for i := 3; i <= n; i++ {
-		prev, curr = curr, prev+curr
-	}
-
-	return curr
 }
