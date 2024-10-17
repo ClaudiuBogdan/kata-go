1b08a4d1899dcf2678253d2569415fae31619056
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-10-17 08:44:21 +0300

    feat(templates): update bubble sort

--- templates/sort/bubble_sort/bubble_sort.go
+++ templates/sort/bubble_sort/bubble_sort.go
@@ -2,21 +2,5 @@ package bubblesort
 
 // BubbleSort performs bubble sort on a slice of integers
 func BubbleSort(arr []int) []int {
-	n := len(arr)
-	swapped := true
 
-	for swapped {
-		swapped = false
-		for i := 1; i < n; i++ {
-			if arr[i-1] > arr[i] {
-				// Swap elements
-				arr[i-1], arr[i] = arr[i], arr[i-1]
-				swapped = true
-			}
-		}
-		// Optimization: after each pass, the largest unsorted element is at the end
-		n--
-	}
-
-	return arr
 }
