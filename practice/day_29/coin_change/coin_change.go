b6dddb2989b09a7caf2950f4dd48517ddfa92d46
Claudiu Constantin Bogdan <claudiu.bogdan@devostack.com>
2024-11-07 10:23:34 +0200

    feat(day29): solve coin change

---
+++ practice/day_29/coin_change/coin_change.go
@@ -0,0 +1,28 @@
+package coinchange
+
+// CoinChange Given an amount of money and an array of coin denominations,
+// find the minimum number of coins needed to make up that amount.
+// If it's not possible to make the amount using the given coin denominations, return -1.
+func CoinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+
+	for i := range dp {
+		dp[i] = amount + 1
+	}
+
+	dp[0] = 0
+
+	for i := 1; i <= amount; i++ {
+		for _, coin := range coins {
+			if coin <= i {
+				dp[i] = min(dp[i], dp[i-coin]+1)
+			}
+		}
+	}
+
+	if dp[amount] > amount {
+		return -1
+	}
+
+	return dp[amount]
+}
